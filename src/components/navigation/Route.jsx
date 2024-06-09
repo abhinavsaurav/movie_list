fc6138f39f7cb899696829824149f5607be37cc1
Abhinav Saurav <abhinavsaurav1@gmail.com>
2024-06-09 17:52:55 +0530

    routing redirection added for default load

--- src/components/navigation/Route.jsx
+++ src/components/navigation/Route.jsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
 import { useNavigation } from "../../hooks/useNavigation";
+import { MOVIE_LIST_PAGE_PATH } from "../../constants/constants";
 
 function Route({routePath, children}) {
-    const { path } = useNavigation();
+    const { path, navigateTo } = useNavigation();
+
+    if(path == "/"){
+        navigateTo(MOVIE_LIST_PAGE_PATH);
+    }
 
     if(path.startsWith(routePath)){
         window.history.pushState({}, '', path);
